1ce4883a2c8b5598381eccaafe973822c641f31a
apisu <aurelien.pisu@cohorte-technologies.com>
2021-05-24 17:41:44 +0200

    add venv for build and fix import

--- yblues/main.py
+++ yblues/main.py
@@ -1,13 +1,22 @@
 import ycappuccino.core
 import ycappuccino.mongo
 import logging
+import argparse
+
 # ------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     # Setup logs
     logging.basicConfig(level=logging.INFO)
+    root_path = None
+    parser = argparse.ArgumentParser(description='argument for yblues application')
+    parser.add_argument('--root-path', dest='root_path', action='store_const',
+                        const=root_path,  default=None,
+                        help='path of application modules and packages')
 
-    ycappuccino.core.init()
+    args = parser.parse_args()
+
+    ycappuccino.core.init(root_path=args.root_path,port=9001)
     # Run the server
     ycappuccino.core.start()
 
