853d1f4b4547ae5c7d39a233166067200625b3bf
apisu <aurelien.pisu@cohorte-technologies.com>
2021-09-08 10:57:26 +0200

    remove ycappuccino to add submodule

--- app/ycappuccino/core/model/utils.py
+++
@@ -1,7 +0,0 @@
-class YDict(object):
-
-    def __init__(self, *a_tuple):
-        for t in a_tuple:
-            if isinstance(t,dict):
-                for k,v in t.items():
-                    setattr(self, k, v)
